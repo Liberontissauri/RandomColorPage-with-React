8d086149bd3f822722b1557dad9feec48c873009
Liberontissauri <jp.liberon@gmail.com>
2021-06-22 18:06:46 +0100

    added a title to the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Background from "./components/background/background.js"
+import TitleText from "./components/titleText/titleText.js"
 
 function App() {
   return (
     <div className="App">
       <Background></Background>
+      <TitleText></TitleText>
     </div>
   );
 }
---
+++ src/components/titleText/titleText.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./titleText.css"
+
+class TitleText extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="titleText-h1">Click to change Colors</h1>
+            </div>
+        )
+    }
+}
+
+export default TitleText
